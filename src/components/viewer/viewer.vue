a964a22bf15f6a212392ce8dde5c95134d412d52
Pavel Karpovich <pavel.karpovich@outlook.com>
2021-09-01 23:13:57 +0200

    wip: create empty viewer component

---
+++ src/components/viewer/viewer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Viewer</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Viewer',
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,17 @@
 <template>
   <q-page class="flex flex-center">
-    Hello
+    <viewer />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import Viewer from '../components/viewer/viewer'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    Viewer
+  }
 })
 </script>
